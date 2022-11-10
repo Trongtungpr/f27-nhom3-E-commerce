e2dc47144d6a2b2d4812dd1ef4cc1faf613175ce
LeDucTung <trongtungpr@gmail.com>
2022-11-10 23:38:54 +0700

    Tung up 101122

--- src/page/auth/login/LoginPage.jsx
+++ src/page/auth/login/LoginPage.jsx
@@ -6,6 +6,7 @@ import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { loginAction } from "../../../stores/slices";
+import "./LoginPage.scss"
 // import { ReactComponent as ReactLogo } from "../../../../svg/Lias.svg";
 
 const emailRegex =
